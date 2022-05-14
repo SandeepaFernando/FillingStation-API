6757a8f523cfeb1b2e6671f3f770ed3f578e2ed3
Rukshan <arkruka@gmail.com>
2022-05-14 19:12:32 +0530

    entry point inital commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
