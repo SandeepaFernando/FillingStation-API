31f670da3be92045bb16f5f3ec9b296cc7e85131
Rukshan <arkruka@gmail.com>
2022-05-21 22:00:55 +0530

    items curd operations

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 const vendorRoutes = require("./routes/vendorRoutes");
 const branchRoutes = require("./routes/branchRoutes");
 const customerRoutes = require("./routes/costomerRoutes");
+const itemRoutes = require("./routes/itemRoutes");
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/vendor", vendorRoutes);
 app.use("/api/branch", branchRoutes);
 app.use("/api/customer", customerRoutes);
+app.use("/api/item", itemRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
