005c32bc986add57e6ed17c854b9f1285c2a0aa2
Rukshan <arkruka@gmail.com>
2022-05-23 21:34:01 +0530

    pump machine route code format, and changed eidt route to update

--- src/routes/pumpMachineRoutes.ts
+++ src/routes/pumpMachineRoutes.ts
@@ -1,40 +1,51 @@
 import { Router } from "express";
-import { createPumpMachine, deletePumpMachine, getPumpMachineById, updatePumpMachine } from "../controllers/pumpMachineController";
-const route = Router()
+import {
+  createPumpMachine,
+  deletePumpMachine,
+  getPumpMachineById,
+  updatePumpMachine,
+} from "../controllers/pumpMachineController";
+const route = Router();
 
-route.post('/create', async(req, res, next) => {
-    const pumpName: string = req.body.pumpName 
-    const meter: number = req.body.meter 
-    const pumpOperator: number = req.body.pumpOperator 
-    const tank: number = req.body.tank 
+route.post("/create", async (req, res, next) => {
+  const pumpName: string = req.body.pumpName;
+  const meter: number = req.body.meter;
+  const pumpOperator: number = req.body.pumpOperator;
+  const tank: number = req.body.tank;
 
-    const request = await createPumpMachine(pumpName, meter, pumpOperator, tank)
-    
-    return res.status(request.status).json(request)
-})
+  const request = await createPumpMachine(pumpName, meter, pumpOperator, tank);
 
-route.post('/edit', async (req, res, next) => {
-    const pumpName: string = req.body.pumpName
-    const meter: number = req.body.meter 
-    const pumpOperator: number = req.body.pumpOperator 
-    const tank: number = req.body.tank 
-    const id: number = req.body.id 
+  return res.status(request.status).json(request);
+});
 
-    const request = await updatePumpMachine(id, pumpName, meter, pumpOperator, tank)
+route.post("/update", async (req, res, next) => {
+  const pumpName: string = req.body.pumpName;
+  const meter: number = req.body.meter;
+  const pumpOperator: number = req.body.pumpOperator;
+  const tank: number = req.body.tank;
+  const id: number = req.body.id;
 
-    return res.status(request.status).json(request)
-})
+  const request = await updatePumpMachine(
+    id,
+    pumpName,
+    meter,
+    pumpOperator,
+    tank
+  );
 
-route.get('/:id', async(req, res, next) => {
-    const id: number = Number(req.params.id)
-    const request = await getPumpMachineById(id)
-    return res.status(request.status).json(request)
-})
+  return res.status(request.status).json(request);
+});
 
-route.post('/delete', async(req, res, next) => {
-    const id: number = req.body.id 
-    const request = await deletePumpMachine(id)
-    return res.status(request.status).json(request)
-})
+route.get("/:id", async (req, res, next) => {
+  const id: number = Number(req.params.id);
+  const request = await getPumpMachineById(id);
+  return res.status(request.status).json(request);
+});
 
-module.exports = route
+route.post("/delete", async (req, res, next) => {
+  const id: number = req.body.id;
+  const request = await deletePumpMachine(id);
+  return res.status(request.status).json(request);
+});
+
+module.exports = route;
